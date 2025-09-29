ce831a3c4b04fa03ccca93d483700caef0e8d434
Filip Mikina <filipmikina@gmail.com>
2025-09-29 15:34:58 +0200

    chore(tui): add help to the profile name picker

--- internal/tui/profile_name_picker.go
+++ internal/tui/profile_name_picker.go
@@ -4,33 +4,61 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/sirupsen/logrus"
 )
 
+type profileNamePickerKeyMap struct {
+	Create key.Binding
+	Back   key.Binding
+}
+
+func (c *profileNamePickerKeyMap) Help() []key.Binding {
+	return []key.Binding{
+		c.Create,
+		c.Back,
+	}
+}
+
 type ProfileNamePicker struct {
 	textInput textinput.Model
 	width     int
 	height    int
+	help      help.Model
+	keyMap    *profileNamePickerKeyMap
 }
 
 func NewProfileNamePicker() *ProfileNamePicker {
 	ti := textinput.New()
 	ti.Placeholder = "New Profile Name"
 	ti.Focus()
-	ti.CharLimit = 156
+	ti.CharLimit = 20
 	ti.Width = 20
 
 	return &ProfileNamePicker{
 		textInput: ti,
+		help:      help.New(),
+		keyMap: &profileNamePickerKeyMap{
+			Create: key.NewBinding(
+				key.WithKeys("enter"),
+				key.WithHelp("enter", "create profile"),
+			),
+			Back: key.NewBinding(
+				key.WithKeys("esc"),
+				key.WithHelp("esc", "return/back"),
+			),
+		},
 	}
 }
 
 func (p *ProfileNamePicker) SetWidth(width int) {
 	p.width = width
 	p.textInput.Width = width - 5
+	p.textInput.CharLimit = width - 5
 }
 
 func (p *ProfileNamePicker) SetHeight(height int) {
@@ -41,10 +69,10 @@ func (p *ProfileNamePicker) Update(msg tea.Msg) tea.Cmd {
 	cmds := []tea.Cmd{}
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "esc":
+		switch {
+		case key.Matches(msg, p.keyMap.Back):
 			cmds = append(cmds, profileNameToogled())
-		case "enter":
+		case key.Matches(msg, p.keyMap.Create):
 			text := p.textInput.Value()
 			// todo validate profile name, no spaces etc, check other profiles here from cfg
 			if text == "" {
@@ -67,9 +95,25 @@ func (p *ProfileNamePicker) Update(msg tea.Msg) tea.Cmd {
 
 func (p *ProfileNamePicker) View() string {
 	sections := []string{}
+	availableSpace := p.height
+	logrus.Debugf("availableSpace for ProfileNamePicker: %d", availableSpace)
+
 	title := TitleStyle.Margin(0, 0, 1, 0).Render("Type the profile name")
-	ti := p.textInput.View()
 	sections = append(sections, title)
+	availableSpace -= lipgloss.Height(title)
+
+	ti := p.textInput.View()
 	sections = append(sections, ti)
+	availableSpace -= lipgloss.Height(ti)
+
+	help := p.help.ShortHelpView(p.keyMap.Help())
+	availableSpace -= lipgloss.Height(help)
+
+	logrus.Debugf("spacer height: %d", availableSpace)
+
+	spacer := lipgloss.NewStyle().Height(availableSpace).Render("")
+	sections = append(sections, spacer)
+	sections = append(sections, help)
+
 	return lipgloss.JoinVertical(lipgloss.Top, sections...)
 }
